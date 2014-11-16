77e3d69dca7ac7e3644d833ce77b8f2a7b6c3a3d
Gabor Szelcsanyi <szelcsanyi.gabor@gmail.com>
2014-11-16 20:07:08 +0100

    new parameters

--- recipes/disk_tuning.rb
+++ recipes/disk_tuning.rb
@@ -8,13 +8,13 @@ disks = node['block_device'].select { |d| d =~ /^xvd/ }
 disks.each do |disk|
 
   if node["virtualization"].any? and node['virtualization']['role'] == 'guest' then
-    sysfs_parameter '/sys/block/#{ disk }/queue/scheduler' do
+    sysfs_parameter 'block/#{ disk }/queue/scheduler' do
       comment 'Set disk scheduler to noop on #{ disk }'
       value 'noop'
     end
   end
 
-  sysfs_parameter '/sys/block/#{ disk }/queue/rq_affinity' do
+  sysfs_parameter 'block/#{ disk }/queue/rq_affinity' do
     comment 'Set disk rq affinity to 2 on #{ disk }'
     value '2'
   end
