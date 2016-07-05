16b2b5af1bb337a1888e20d5173db4d2f49e09e2
szelcsanyi <szelcsanyi.gabor@gmail.com>
2016-07-05 11:35:13 +0200

    set deadline scheduler for non rotating disks

--- recipes/disk_tuning.rb
+++ recipes/disk_tuning.rb
@@ -2,18 +2,23 @@
 # Cookbook Name:: L7-sysfs
 # Recipe:: disk_tuning
 #
-# Copyright 2015, Gabor Szelcsanyi <szelcsanyi.gabor@gmail.com>
+# Copyright 2016, Gabor Szelcsanyi <szelcsanyi.gabor@gmail.com>
 
 # Not in openvz guest
 return if File.exist?('/proc/vz')
 
-disks = node['block_device'].select { |d| d =~ /^xvd/ }
+disks = node['block_device'].select { |d| d =~ /^x?(v|s)d[a-z]/ }
 disks.each do |disk|
 
   if node['virtualization'].any? && node['virtualization']['role'] == 'guest'
     L7_sysfs "block/#{ disk.at(0) }/queue/scheduler" do
       comment "Set disk scheduler to noop on #{ disk.at(0) }"
       value 'noop'
+    end
+  elsif File.exist?("/sys/block/#{ disk.at(0) }/queue/rotational") and File.read("/sys/block/#{ disk.at(0) }/queue/rotational")=='0'
+    L7_sysfs "block/#{ disk.at(0) }/queue/scheduler" do
+      comment "Set disk scheduler to deadline on #{ disk.at(0) }"
+      value 'deadline'
     end
   end
 
